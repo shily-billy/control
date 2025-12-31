92749a414232bc9ba540fbdfbc39445bb38118bc
shily-billy <rashidyshayan4367@gmail.com>
2025-12-31 19:24:17 +0330

    Initialize affiliate agents

---
+++ control/agents/affiliate/__init__.py
@@ -0,0 +1,11 @@
+"""Affiliate agents module"""
+
+from .manamod_agent import ManamodAgent
+from .mihanstore_agent import MihanStoreAgent
+from .memarket_agent import MemarketAgent
+
+__all__ = [
+    'ManamodAgent',
+    'MihanStoreAgent',
+    'MemarketAgent'
+]
