6ba9294d104b67e515849bcbecd0b81ba895399c
shily-billy <rashidyshayan4367@gmail.com>
2025-12-31 19:24:52 +0330

    Initialize social media agents

---
+++ control/agents/social/__init__.py
@@ -0,0 +1,11 @@
+"""Social media agents module"""
+
+from .instagram_agent import InstagramAgent
+from .twitter_agent import TwitterAgent
+from .tiktok_agent import TikTokAgent
+
+__all__ = [
+    'InstagramAgent',
+    'TwitterAgent',
+    'TikTokAgent'
+]
