67e226ace8b258ab3cdaf85fdda5cf01f95ecfc0
shily-billy <rashidyshayan4367@gmail.com>
2025-12-31 19:22:18 +0330

    Initialize marketplace agents

---
+++ control/agents/marketplace/__init__.py
@@ -0,0 +1,15 @@
+"""Marketplace agents module"""
+
+from .divar_agent import DivarAgent
+from .sheypoor_agent import SheypoorAgent
+from .pindo_agent import PindoAgent
+from .basalam_agent import BasalamAgent
+from .torob_agent import TorobAgent
+
+__all__ = [
+    'DivarAgent',
+    'SheypoorAgent',
+    'PindoAgent',
+    'BasalamAgent',
+    'TorobAgent'
+]
